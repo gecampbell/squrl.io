5f4debf651cda45a834f4270e06c11535b74007d
gecampbell <glen@broadpool.com>
2013-12-19 14:48:13 -0600

    added small page

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
-<?php
+<!DOCTYPE html><?php
 /**
  * The main home page for SQURL.IO
  *
  * @copyright 2013 Glen Campbell. See LICENSE for details.
  */
+?>
+<html>
+
+<head>
+<title>squrl.io</title>
+<style>
+body {
+	margin: 1em auto;
+	max-width: 60em;
+	font-family: Helvetica, Arial, Sans-Serif;
+}
+h1 {
+	font-weight: 100;
+}
+h1 em {
+	font-weight: 900;
+}
+</style>
+</head>
+
+<body>
+<h1><em>squrl.io</em> url storage</h1>
+<p>Under Construction</p>
+</body>
+
+</html>
